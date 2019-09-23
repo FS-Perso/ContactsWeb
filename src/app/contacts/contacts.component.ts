d60b0f9a0f062159ab8496ef431f71d9f6f7ca63
Fsalah <fedi.salah.perso@gmail.com>
2019-09-23 13:36:14 +0200

    Afficher les données qui sont insérées dans le nouveau composant 'ContactsComponent'

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  contact = { name: "Fedi-contact", email: "fedi-contact@gmail.com" }
+
   constructor() { }
 
   ngOnInit() {
