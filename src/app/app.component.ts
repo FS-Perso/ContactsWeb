05476c2030fa895031ebc805abf93552487a9e9a
Fsalah <fedi.salah.perso@gmail.com>
2019-09-23 12:13:50 +0200

    Affichage dynamique du nom et email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ContactsWeb';
+  contact = { nom: "Fedi", email: "fedi@gmail.com" }
 }
